d5862c54eff9e5813c3c7911024fbc56c407203b
Adeir <adeir@uwo.ca>
2023-11-08 12:06:28 -0500

    added more input sanitization and fixed some bugs

--- client/script.js
+++ client/script.js
@@ -1,10 +1,12 @@
+// Event listener for DOMContentLoaded ensures that the script runs after the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     const searchButton = document.getElementById('searchButton');
-    searchButton.onclick = function() {
-        searchSuperheroes();
-        
+    // Event listener for the search button click
+        searchButton.onclick = function() {
+        searchSuperheroes(); // Function to initiate superhero search
     };
 });
+// Event listener for DOMContentLoaded ensures that the script runs after the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     const clearButton = document.getElementById('clearSearch');
     clearButton.onclick = function() {
@@ -13,14 +15,14 @@ document.addEventListener('DOMContentLoaded', function () {
         
     };
 });
-
+// Event listener for DOMContentLoaded ensures that the script runs after the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     const publisherButton = document.getElementById('displayPublishers');
     publisherButton.onclick = function() {
         showPublisher();
     };
 });
-
+// Event listener for DOMContentLoaded ensures that the script runs after the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     const infoCheckbox = document.getElementById('supInfoBox');
     const powersCheckbox = document.getElementById('supPowerBox');
@@ -39,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 let currentDisplayedHeros ="";
 
-
+// Event listener for DOMContentLoaded ensures that the script runs after the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
     const searchType = document.getElementById('searchType');
     const checkBoxes = document.getElementById('checkboxSection');
@@ -73,34 +75,43 @@ function showPublisher(){
 
 
 
+// Function that searches for superheroes based on user input
 function searchSuperheroes() {
-    const searchField = document.getElementById('searchField').value;
-    searchField = escapeHTML(searchField);//sanitize the user input
+    // Sanitize the search field input to prevent XSS
+    let searchField = sanitizeString(document.getElementById('searchField').value);
+    console.log(searchField);
     const searchType = document.getElementById('searchType').value;
+    console.log(searchType);
     const searchNumValue = document.getElementById('nSearch').value;
+    // Ensure the search number is a valid number or undefined
     const searchNum = getNumericValueOrUndefined(searchNumValue);
+    console.log(searchNum);
 
-    if(searchType === 'id'){
-       searchId(searchField);
-    }else{
-         // Assuming '/api/superhero-search'
-    const urlSearch = new URL('/api/superhero-search', window.location.origin);
-
-    if(searchNum ===0){
+    if (searchType === 'id') {
+        // If the search type is 'id', sanitize the input and search by ID
+        searchField = encodeURIComponent(searchField);
+        searchId(searchField);
+    } else {
+        // For other search types, construct the URL with sanitized and encoded parameters
+        const urlSearch = new URL('/api/superhero-search', window.location.origin);
+        if(searchNum ===0){
         const params = { field: searchType, pattern: searchField};
         urlSearch.search = new URLSearchParams(params).toString();
         
+        fetch(urlSearch) 
+        .then(response => response.json())
+        .then(heros => displayHeros(heros))
+        .catch(error => console.error('Error:', error));
         }else{
-          const params = { field: searchType, pattern: searchField, n: searchNum };
-          console.log(searchType,searchField, searchNum);
-          urlSearch.search = new URLSearchParams(params).toString();  
-        }
-    
-    fetch(urlSearch) 
-    .then(response => response.json())
-    .then(heros => displayHeros(heros))
-    .catch(error => console.error('Error:', error)); 
+        const params = { field: searchType, pattern: searchField, n: searchNum };
+        urlSearch.search = new URLSearchParams(params).toString();
+        
+        fetch(urlSearch) 
+        .then(response => response.json())
+        .then(heros => displayHeros(heros))
+        .catch(error => console.error('Error:', error)); 
     }
+}
 }
 //function that checks to make sure the user input was a number, if it was not then the value is set to undefined
 function getNumericValueOrUndefined(value) {
@@ -127,6 +138,7 @@ function searchId(id){
 
 }
 function displayHerosId(heros){
+    
     const resultsContainer = document.getElementById('searchResults');
     resultsContainer.innerHTML = ''; // Clear previous results -- ok to use this as it is not user generated content
     const list = document.createElement('ul');
@@ -145,6 +157,7 @@ function displayHerosId(heros){
 }
 
 function displayHeros(heros){
+    console.log(heros);
     const resultsContainer = document.getElementById('searchResults');
     resultsContainer.innerHTML = ''; // Clear previous results
     currentDisplayedHeros = heros;
@@ -161,7 +174,7 @@ function displayHeros(heros){
         // Create a card for each hero
         const card = document.createElement('div');
         card.className = 'card';
-
+        //loop through the attributes for each hero
         for(let key in hero){
             if(hero.hasOwnProperty(key)){
                 // Create a paragraph for each attribute
@@ -174,16 +187,17 @@ function displayHeros(heros){
         // Append the card to the container
         cardsContainer.appendChild(card);
     });
-
+    //append all the cards to the results
     resultsContainer.appendChild(cardsContainer);
 }
 
 
-
+//function responsible for displaying the publishers
 function displayPublishers(publishers) {
     const resultsContainer = document.getElementById('searchResults');
     resultsContainer.innerHTML = ''; // Clear previous results
     
+    //if no publsihers are found
     if (publishers.length === 0) {
         resultsContainer.innerText = 'No publishers found.';
         return;
@@ -191,35 +205,33 @@ function displayPublishers(publishers) {
 
     // Create a list to display the results
     const list = document.createElement('ul');
+    //go through the publishers and display each of the publishers
     publishers.forEach(publisher => {
         const listItem = document.createElement('li');
         listItem.textContent = publisher || 'Not specified'; // Display 'Not specified' if the publisher is an empty string
         list.appendChild(listItem);
     });
-
+//append the results
     resultsContainer.appendChild(list);
 }
 
 
 
 //List functionality
-
+//function to get all the saved lists from the server
 function getSavedLists(){
-
+//route to get the saved lists
     const urlSearchLists = new URL('/api/lists', window.location.origin);
     fetch(urlSearchLists) 
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        // Assuming 'data' is an object where keys are list names
         const listsContainer = document.getElementById('list-display');
         listsContainer.innerHTML = ''; // Clear existing content
 
-        // Iterate over each list
+        // Iterate over each list and dispay the name of the list
         data.forEach(listName => {
             const listItem = document.createElement('div');
             listItem.textContent = listName;
-            // Optionally, you can add buttons or links for other actions (delete, view, etc.)
             listsContainer.appendChild(listItem);
         });
     })
@@ -227,17 +239,34 @@ function getSavedLists(){
 }
 
 
+// Function to sanitize input to protect against XSS
+function sanitizeString(str) {
+    const temp = document.createElement('div');
+    temp.textContent = str;
+    return temp.innerHTML;
+}
 
+// Function to validate that the ID is a proper number
+function validateAndParseIds(idsString) {
+    // Split the string by commas and filter out any non-numeric values
+    return idsString.split(',')
+        .map(id => id.trim()) // Remove whitespace
+        .filter(id => id !== '' && !isNaN(id)) // Keep only valid numbers
+        .map(Number); // Convert to numbers
+}
+
+// Function that creates a new list
 function createList() {
-    const listName = document.getElementById('listNameInput').value; // Get the list name from the input field
-    const idsToAdd = document.getElementById('listIdInput').value;
+    let listName = document.getElementById('listNameInput').value; // Get the list name from the input field
+    let idsToAdd = document.getElementById('listIdInput').value;
 
-    // Only split the idsToAdd and map to integers if it's not an empty string
-    const idsArray = idsToAdd ? idsToAdd.split(',').map(id => parseInt(id.trim(), 10)) : [];
+    // Sanitize inputs
+    listName = sanitizeString(listName);
+    const idsArray = validateAndParseIds(idsToAdd);
 
-    // Check if the listName is not empty
+    // Check if the listName is not empty after sanitization
     if (!listName.trim()) {
-        alert('Please enter a list name.');
+        alert('Please enter a valid list name.');
         return;
     }
 
@@ -253,7 +282,7 @@ function createList() {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ superheroIds: idsArray }) // Send superhero IDs as part of the request body
+        body: JSON.stringify({ superheroIds: idsArray }) // Send sanitized superhero IDs as part of the request body
     })
     .then(response => {
         if (!response.ok) {
@@ -275,8 +304,10 @@ function createList() {
 
 
 
+//function to search for a list
 function searchList() {
     const listName = document.getElementById('searchListInput').value;
+    listName = sanitizeString(listName);//sanitize user input
   const resultsContainer = document.getElementById('list-display');
   resultsContainer.innerHTML = ''; // Clear previous results
   const baseURL = `http://localhost:3000/api/lists/${listName}`;
@@ -286,6 +317,7 @@ function searchList() {
   const displayInfoChecked = document.getElementById('displayInfo').checked;
   const displayPowersChecked = document.getElementById('displayPowers').checked;
 
+  //based on what check box is selected we search accordingly
   if (displayIdsChecked) {
     fetch(`${baseURL}/ids`)
       .then(response => response.json())
@@ -317,7 +349,7 @@ function searchList() {
     idsElement.innerHTML = `<h3>ID List:</h3><p>${ids.join(', ')}</p>`;
     document.getElementById('list-display').appendChild(idsElement);
   }
-  
+  //fucntion to display list info
   function displayListInfo(info) {
     const cardsContainer = document.createElement('div');
     cardsContainer.className = 'cards-container';
@@ -327,7 +359,7 @@ function searchList() {
     });
     document.getElementById('list-display').appendChild(cardsContainer);
   }
-  
+  //function to display powers
   function displayPowers(powersArray) {
     // Assuming powersArray is an array of objects where each object contains the powers of a hero
     const powersElement = document.createElement('div');
@@ -346,7 +378,7 @@ function searchList() {
     powersElement.innerHTML = `<h3>Superhero Powers:</h3><ul>${powersListHTML}</ul>`;
     document.getElementById('list-display').appendChild(powersElement);
   }
-  
+  //create a hero card for formating with css
   function createHeroCard(hero) {
     const card = document.createElement('div');
     card.className = 'card';
